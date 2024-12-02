48e5fb8712efa5617ac0ca14b0dec194fcc616da
Ecssooo <alexandre.lejeune9@gmail.com>
2024-12-02 15:36:35 +0100

    Setup SFML

--- ProjetCPP/ProjetCPP.cpp
+++ ProjetCPP/ProjetCPP.cpp
@@ -1,6 +1,24 @@
+#include <SFML/Graphics.hpp>*
 
 int main(int argc, char* argv[])
 {
-    
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+
+    while (window.isOpen()) {
+       
+        window.clear();
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if ((event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
+
+        window.display();
+    }
+
     return 0;
 }
